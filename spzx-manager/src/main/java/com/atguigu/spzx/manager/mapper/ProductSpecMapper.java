09d33a9752c83802b6c5c3ff05a7d0836fd3e7d7
Wu Hanfeng <dankejun0.0@gmail.com>
2025-08-05 10:08:59 +0800

    feat: 商品规格管理

---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/mapper/ProductSpecMapper.java
@@ -0,0 +1,22 @@
+package com.atguigu.spzx.manager.mapper;
+
+import com.atguigu.spzx.model.entity.product.ProductSpec;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * Time: 2025/8/5
+ * Author: Dankejun
+ * Description:
+ */
+@Mapper
+public interface ProductSpecMapper {
+    List<ProductSpec> findByPage();
+
+    void save(ProductSpec productSpec);
+
+    void updateById(ProductSpec productSpec);
+
+    void deleteById(Long id);
+}
---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/ProductSpecService.java
@@ -0,0 +1,19 @@
+package com.atguigu.spzx.manager.service;
+
+import com.atguigu.spzx.model.entity.product.ProductSpec;
+import com.github.pagehelper.PageInfo;
+
+/**
+ * Time: 2025/8/5
+ * Author: Dankejun
+ * Description:
+ */
+public interface ProductSpecService {
+    PageInfo<ProductSpec> findByPage(Integer page, Integer limit);
+
+    void save(ProductSpec productSpec);
+
+    void updateById(ProductSpec productSpec);
+
+    void deleteById(Long id);
+}
